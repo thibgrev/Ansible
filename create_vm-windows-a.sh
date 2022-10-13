8e3613ba6bfc42f355e236aecc4b53b5c38dc4df
thibgrev <thibgrev@hotmail.com>
2022-10-13 17:15:32 +0200

    Ajout de play pour Windows

--- create_vm-windows-a.sh
+++ create_vm-windows-a.sh
@@ -1,1 +1,1 @@
-ansible-playbook -i inventory_ansible Playbook/vmware_deploy_VM.yml --ask-vault-password --extra-vars "nomVM=vm-windows-a" 
+ansible-playbook -i inventory_ansible Playbook/vmware_deploy_VM.yml --ask-vault-password --extra-vars "nomVM=vm-windows-a, memory_mb=4096"
