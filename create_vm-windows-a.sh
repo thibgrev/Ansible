384a7bb95b874c2440ae88c522e88b16517db1de
“thibgrev@hotmail.com” <thibgrev@hotmail.com>
2023-04-19 16:28:28 +0200

    Issue repared - comm to windows server OK

--- create_vm-windows-a.sh
+++ create_vm-windows-a.sh
@@ -1,1 +1,1 @@
-ansible-playbook -i inventory_ansible Playbook/vmware-deploy_VM.yml --ask-vault-password --extra-vars "VMname=vm-redhat-a"
+ansible-playbook Playbook/vmware-deploy_VM.yml --ask-vault-password --extra-vars "VMname=vm-redhat-a"
